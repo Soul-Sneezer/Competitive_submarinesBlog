46057950b44bfb68c4bdc271ea1e2dd466b11411
Robert Petre <petrerobertcristian@gmail.com>
2024-06-03 15:24:28 +0300

    Sync

--- blog0.js
+++ blog0.js
@@ -1,6 +1,12 @@
-var article = document.getElementsByTagName("article")[0];
-var fs = require("fs");
-var content = fs.readFileSync("./content0.tex", "utf-8");
+var a = document.querySelectorAll("article");
 var par = document.createElement("p")
-par.innerHTML = content;
-article.appendChild(par);
+console.log(a)
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://localhost:8000/content0.tex");
+xhr.onreadystatechange = function() {
+	console.log(xhr.responseText);
+	par.innerHTML = xhr.responseText;
+}
+xhr.send();
+//par.innerHTML = content;
+//a.appendChild(par);
