6d149e145c516613724434d6d3df92af3b4993bd
Soul Sneezer <petrerobertcristian@gmail.com>
2024-06-03 03:38:44 +0300

    Started work on second part of project

---
+++ blog0.js
@@ -0,0 +1,19 @@
+function fetchJSONData() {
+	fetch("http://localhost:8000/post0.json")
+							.then((res) => {
+									if (!res.ok) {
+											throw new Error
+													(`HTTP error! Status: ${res.status}`);
+									}
+									return res.json();
+							})
+							.then((data) => 
+										console.log(data))
+							.catch((error) => 
+										 console.error("Unable to fetch data:", error));
+			}
+
+var file = fetchJSONData();
+var s = JSON.parse(file);
+var article = document.getElementsByTagName("article")[0];
+article.innerHtml = s.content;
