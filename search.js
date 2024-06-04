b6ae27d5ccdfc832a090511ea2f87ae869348010
Soul Sneezer <petrerobertcristian@gmail.com>
2024-06-04 19:26:06 +0300

    Minor progress

--- search.js
+++ search.js
@@ -1,3 +1,26 @@
+function sendFormularData()
+{
+	var formular = document.querySelectorAll("#search input")[0];
+	console.log(formular.value);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "https://competitivesubmarines.com/");
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+	xhr.onreadystatechange = () => {
+		if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200)
+		{
+		}
+	}	
+
+	xhr.send(formular.value);
+
+}
+
 window.onload = function ()
 {
+	document.addEventListener('keypress', function(event) {
+		if(event.keyCode == 13 || event.which == 13)
+			sendFormularData();
+	}, true);
 }
