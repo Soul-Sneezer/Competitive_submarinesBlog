2f8d84030e82ff14ce9e837760a4d754afa87170
Soul Sneezer <petrerobertcristian@gmail.com>
2024-06-04 23:12:00 +0300

    Finished responsive search submarine

--- search.js
+++ search.js
@@ -19,8 +19,27 @@ function sendFormularData()
 
 window.onload = function ()
 {
+	var sub = document.querySelectorAll(".search_sub .submarine__body")[0];
+	var propeller = document.querySelectorAll(".submarine__propeller")[0];
+	var bubbles = document.querySelectorAll(".bubbles__container")[0];
+	var formular = document.querySelectorAll("#search input")[0];
+	var size = Math.max(formular.textLength * 20, 250);
+	
+	sub.style.width = size + "px";
+	formular.style.width = size - 50 + "px";
+	propeller.style.left = 320 + (size - 250) / 2 + "px";
+	bubbles.style.left = 255 + (size - 250) / 2 + "px";
+
 	document.addEventListener('keypress', function(event) {
 		if(event.keyCode == 13 || event.which == 13)
 			sendFormularData();
+		else
+		{
+			var size = Math.max(formular.textLength * 20, 250);
+			sub.style.width = size + "px";
+			formular.style.width = size - 50 + "px";
+			propeller.style.left = 320 + (size - 250) / 2 + "px"
+			bubbles.style.left = 255 + (size - 250) / 2 + "px";
+		}
 	}, true);
 }
