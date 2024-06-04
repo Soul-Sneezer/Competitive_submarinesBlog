4df81028755bdfbc42708657244044416e80bdc4
Soul Sneezer <petrerobertcristian@gmail.com>
2024-06-04 23:21:25 +0300

    Update sub size on backspace

--- search.js
+++ search.js
@@ -42,4 +42,16 @@ window.onload = function ()
 			bubbles.style.left = 255 + (size - 250) / 2 + "px";
 		}
 	}, true);
+
+	document.addEventListener("keyup", function(event)
+		{
+			if(event.key === "Backspace")
+			{
+				var size = Math.max(formular.textLength * 20, 250);
+				sub.style.width = size + "px";
+				formular.style.width = size - 50 + "px";
+				propeller.style.left = 320 + (size - 250) / 2 + "px"
+				bubbles.style.left = 255 + (size - 250) / 2 + "px";
+			}
+		});
 }
